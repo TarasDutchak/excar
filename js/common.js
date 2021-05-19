719c717032b1cadf2c720aa839844c53fe02bcd2
TarasDutchak <tarasdutchak90@gmail.com>
2021-05-19 20:49:28 +0300

    update

--- js/common.js
+++ js/common.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-    
+    // parallax
     var scene = document.getElementById('scene');
     var parallaxInstance = new Parallax(scene);
+
+    // accordeon plus minus
+
+    $('.accordeon-title').click(function(){
+        $('.accordeon-title').not($(this)).removeClass('minus');
+        $(this).toggleClass('minus');
+    });
+
+    // to top
+    $('.totop').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+
+
 });
