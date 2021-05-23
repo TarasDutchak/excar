0c42a3183d5563854ea5e1991766ae8f3e8c284f
TarasDutchak <tarasdutchak90@gmail.com>
2021-05-23 12:52:17 +0300

    update

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     // parallax
-    if($('body *').is('#scene')) {
+    if ($('body *').is('#scene')) {
         var scene = document.getElementById('scene');
         var parallaxInstance = new Parallax(scene);
     }
@@ -20,45 +20,32 @@ $(document).ready(function () {
     });
 
     // tablet slider index page
+    if ($('body *').is('.servicesdark__slider')) {
+        function mobileOnlySlider() {
+            $('.servicesdark__slider').slick({
+                infinite: true,
+                speed: 700,
+                slidesToShow: 1,
+                variableWidth: true,
+                dots: false,
+                nextArrow: '<button type="button" class="slick-next"></button>',
+                prevArrow: '<button type="button" class="slick-prev"></button>',
+                responsive: [{
+                    breakpoint: 1024,
+                    settings: {},
+                    breakpoint: 576,
+                    settings: {
+                        variableWidth: false,
+                        slidesToShow: 1,
+                    }
+                }]
+            });
+        }
 
-    function mobileOnlySlider() {
-        $('.servicesdark__slider').slick({
-            infinite: true,
-                    speed: 700,
-                    slidesToShow: 1,
-                    variableWidth: true,
-                    dots: false,
-                    nextArrow: '<button type="button" class="slick-next"></button>',
-                    prevArrow: '<button type="button" class="slick-prev"></button>',
-                    responsive: [{
-                        breakpoint: 1024,
-                        settings: {
-                        },
-                        breakpoint: 576,
-                        settings: {
-                            variableWidth: false,
-                            slidesToShow: 1,
-                        }
-                    }]
-        });
-    }
-
-    if (window.innerWidth > 1200) {
-        mobileOnlySlider();
-    }
-
-    if (window.innerWidth < 1200) {
-        if (!$('.servicesdark__slider').hasClass('slick-initialized')) {
+        if (window.innerWidth > 1200) {
             mobileOnlySlider();
         }
 
-    } else {
-        if ($('.servicesdark__slider').hasClass('slick-initialized')) {
-            $('.servicesdark__slider').slick('unslick');
-        }
-    }
-
-    $(window).resize(function (e) {
         if (window.innerWidth < 1200) {
             if (!$('.servicesdark__slider').hasClass('slick-initialized')) {
                 mobileOnlySlider();
@@ -69,24 +56,101 @@ $(document).ready(function () {
                 $('.servicesdark__slider').slick('unslick');
             }
         }
-    });
+
+        $(window).resize(function (e) {
+            if (window.innerWidth < 1200) {
+                if (!$('.servicesdark__slider').hasClass('slick-initialized')) {
+                    mobileOnlySlider();
+                }
+
+            } else {
+                if ($('.servicesdark__slider').hasClass('slick-initialized')) {
+                    $('.servicesdark__slider').slick('unslick');
+                }
+            }
+        });
+    }
+
 
     // mob menu
 
-    $('.burgericon').click(function(){
+    $('.burgericon').click(function () {
         $('.mobile-menu').addClass('show');
     });
-    $('.closemenu').click(function(){
+    $('.closemenu').click(function () {
         $('.mobile-menu').removeClass('show');
     });
 
+    // custom selects
 
+    if ($('body *').is('select')) {
+        $(function () {
+            $('select').styler();
+        });
+    }
+    
+    // RANGE SLIDER
 
+        $('.noUi-handle').on('click', function() {
+          $(this).width(50);
+        });
+        var rangeSlider = document.getElementById('slider-range');
+        var moneyFormat = wNumb({
+          decimals: 0,
+          thousand: ',',
+          prefix: '$ '
+        });
+        noUiSlider.create(rangeSlider, {
+          start: [5000, 200000],
+          step: 1,
+          range: {
+            'min': [5000],
+            'max': [200000]
+          },
+          format: moneyFormat,
+          connect: true
+        });
+        
+        // Set visual min and max values and also update value hidden form inputs
+        rangeSlider.noUiSlider.on('update', function(values, handle) {
+          document.getElementById('slider-range-value1').innerHTML = values[0];
+          document.getElementById('slider-range-value2').innerHTML = values[1];
+          document.getElementsByName('min-value').value = moneyFormat.from(
+            values[0]);
+          document.getElementsByName('max-value').value = moneyFormat.from(
+            values[1]);
+        });
+      
+        // catalog selects
+        $('.selectimitation__selitem').click(function(){
+            $(this).toggleClass('open');
+            $(this).next('.selectimitation__sellist').toggle();
+        });
 
-
-
-
-
+        $(document).click(function (event) {
+            let $target = $(event.target);
+            if (!$target.closest('.selectimitation').length) {
+                $('.selectimitation__sellist').hide();
+                $('.selectimitation__selitem').removeClass('open');
+            }
+        });
+        // текст вибраного інпута
+        $('.selectimitation__checkrow label').click(function(){
+            var textLab = $(this).text();
+            $('.selectimitation__selitem span:not(.selectimitation__arrow)').text(textLab);
+        });
+        // вирубаєм всі інпути якщо вибрали "всі бренди"
+        $('#allbarands').change(function(){
+            if ($(this).is(':checked')) {
+                $('.selectimitation__checkrow input').not($(this)).prop('checked', false)
+            }
+        });
+        // якщо вибитраєм модель то "всі бренди" вирубаємо
+        $('.selectimitation__checkrow input:not(#allbarands)').change(function(){
+            if ($(this).is(':checked')) {
+                $('.selectimitation__checkrow input#allbarands').prop('checked', false)
+            }
+        });
 
 
 });
